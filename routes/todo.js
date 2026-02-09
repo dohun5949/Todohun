25edaa85d42830bdc4691551606d718a0a1a7626
dohun5949 <rlaehgns5949@gmail.com>
2026-02-09 15:13:42 +0900

    달력까지

--- routes/todo.js
+++ routes/todo.js
@@ -1,5 +1,104 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const db = require("../db");
 
+router.post("/done", async (req, res) => {
+    console.log(req.session.user);
+    if (!req.session.user) {
+        return res.status(401).json({ message: "로그인 필요" });
+    }
+    const { text, category } = req.body;
+    const user_id = req.session.user.user_id;
+    if (!text || !category) {
+        return res.status(400).json({ message: "필수 값 누락" });
+    }
+    const sql = `insert into todo.todo (text,category, user_id, completed,date) values(?,?,?,0,CURDATE())`;
+    db.query(sql, [text, category, user_id], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "DB error" });
+        }
+        res.send("할 일 등록 완료");
+    })
+});
+router.get("/todolist", async (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ message: "로그인 필요" });
+    }
 
+    const user_id = req.session.user.user_id;
+    const sql = `select * from todo.todo where user_id = ? and date = CURDATE() order by completed, created_at desc`;
+    db.query(sql, [user_id], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "DB error" });
+        }
+
+        res.json(results);
+    })
+})
+router.post("/del", async (req, res) => {
+    const { text_id } = req.body;
+    const user_id = req.session.user.user_id;
+    if (!text_id) {
+        return res.status(400).json({ message: "text_id 없음" });
+    }
+    const sql = `delete from todo.todo where text_id = ? and user_id = ?`;
+    db.query(sql, [text_id, user_id], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "DB 오류" });
+        }
+        res.json({ success: true });
+    })
+})
+router.post("/completed", async (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ message: "로그인 필요" });
+    }
+    const { text_id, completed } = req.body;
+    const user_id = req.session.user.user_id;
+    if (text_id === undefined || completed === undefined) {
+        return res.status(400).json({ message: "값 없음" });
+    }
+    const sql = `update todo.todo set completed = ? where text_id = ? and user_id = ?`;
+    db.query(sql, [completed, text_id, user_id], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "DB 오류" });
+        }
+        res.json({ success: true });
+    })
+})
+router.get("/remain", async (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ message: "need to login" });
+    }
+    
+    const user_id = req.session.user.user_id;
+
+    const sql = `select * from todo.todo where user_id = ? and completed = 0`;
+    db.query(sql, [user_id], (err,results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "DB error" });
+        }
+        res.json(results);
+    });
+})
+router.get("/calender", async(req,res)=>{
+    if(!req.session.user){
+        return res.status(401).json({message: "need to login"});
+    }
+    const user_id = req.session.user.user_id;
+    const sql = `select text_id, text, category, date,completed from todo.todo where user_id = ?`;
+    db.query(sql, [user_id], (err,results)=>{
+        if(err){
+            console.error(err);
+            return res.status(500).json({message: "DB error"});
+        }
+        res.json(results);
+    })
+})
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -20,11 +20,11 @@ router.post("/signup", async (req, res) => {
             if (err) {
                 console.error("회원가입 실패");
                 if (err.code === 'ER_DUP_ENTRY') {
-                    return res.status(409).send("이미 존재하는 아이디 또는 이메일입니다.");
+                    return res.status(409).json({message : "이미 존재하는 아이디 또는 이메일입니다."});
                 }
                 console.error("MySQL 오류 코드:", err.code);
                 console.error("MySQL 오류 메시지:", err.message);
-                return res.status(500).send("회원가입 실패 (DB 오류)");
+                return res.status(500).json({message: "회원가입 실패 (DB 오류)"});
             }
             res.send("회원가입 완료!");
         });
@@ -37,16 +37,16 @@ router.post("/signup", async (req, res) => {
 router.post("/unique", async (req, res) => {
     const { login_id } = req.body;
     if (!login_id) {
-        return res.status(400).send("아이디를 입력하세요");
+        return res.status(400).json({message : "아이디를 입력하세요"});
     }
     const sql = `select 1 from todo.user where login_id = ?`;
     db.query(sql, [login_id], (err, results) => {
         if (err) {
             console.error("중복 확인 오류:", err);
-            return res.status(500).send("DB 오류");
+            return res.status(500).json({message: "DB 오류"});
         }
         if (results.length > 0) {
-            return res.status(400).send("이미 존재하는 아이디입니다.");
+            return res.status(400).json({message: "이미 존재하는 아이디입니다."});
         }
         res.send("사용 가능");
     });
@@ -55,88 +55,87 @@ router.post("/login", async (req, res) => {
     console.log(req.body);
     const { login_id, pwd } = req.body;
     if (!login_id || !pwd) {
-        return res.status(400).send("필드를 채워주세요");
+        return res.status(400).json({message: "필드를 채워주세요"});
     }
     const sql = `select * from todo.user where login_id = ?`;
     db.query(sql, [login_id], async (err, results) => {
         if (err) {
             console.error("로그인 쿼리 오류:", err);
-            return res.status(500).send("로그인 실패");
+            return res.status(500).json({message: "로그인 실패"});
         }
         if (results.length === 0) {
-            return res.status(400).send("아이디가 존재하지 않습니다.");
+            return res.status(400).json({message: "아이디가 존재하지 않습니다."});
         }
         const user = results[0];
         const password = await bcrypt.compare(pwd, user.pwd);
         if (!password) {
-            return res.status(401).send("비밀번호가 일치하지 않습니다.")
+            return res.status(401).json({message: "비밀번호가 일치하지 않습니다."});
         }
-        req.session.login_id = user.login_id;
+        req.session.user = {
+            user_id: user.user_id,
+            login_id: user.login_id,
+            nickname: user.nickname
+        };
         res.redirect("/main.html");
     })
 });
 router.post("/myinfo_edit", async (req, res) => {
     const { nickname, writing } = req.body;
-    const login_id = req.session.login_id;
+    const login_id = req.session.user.login_id;
     if (!nickname || !writing) {
-        return res.status(400).send("필드를 채워주세요");
+        return res.status(400).json({message : "필드를 채워주세요"});
     }
     const sql = `update todo.user set nickname = ?, writing = ? where login_id = ?`;
     db.query(sql, [nickname,writing,login_id], async (err, results) => {
         if (err) {
             console.error("정보 수정 오류", err);
-            return res.status(500).send("edit failure");
+            return res.status(500).json({message: "edit failure"});
         }
         if (results.affectedRows === 0) {
-            return res.send(400).status("아이디가 존재하지 않습니다.");
+            return res.status(400).json({message: "아이디가 존재하지 않습니다."});
         }
         res.send({success: true});
     })
 });
 router.get("/myinfo",async(req,res)=>{
-    const login_id = req.session.login_id;
-    if(!login_id){
-        alert("need to login")
-        return;
+    if(!req.session.user){
+        return res.status(401).json({message:"need to login"});
     }
+    const login_id = req.session.user.login_id;
+    
     const sql = `select nickname, email, writing from todo.user where login_id = ?`;
     db.query(sql,[login_id],(err,results)=>{
         if(err){
             console.error("loda info error");
-            alert("load info error");
-            return;
+            return res.status(500).json({message: "load info error"});
         }
         if(results.length === 0){
-            alert("no user");
-            return;
+            return res.status(400).json({message: "no user"});
         }
         res.json(results[0]);
     })
     
 });
 router.post("/withdraw", (req,res)=>{
-    const login_id = req.session.login_id;
+    const login_id = req.session.user.login_id;
     const {nickname} = req.body;
     const sql = `select nickname from todo.user where login_id = ?`;
     db.query(sql,[login_id],(err,results)=>{
         if(err){
             console.log("load info error");
-            return;
+            return res.status(500).json({message: "load info error"});
         }
         if(results.length === 0){
-            alert("no user");
-            return;
+            return res.status(404).json({message: "no user"});
         }
         if(results[0].nickname !== nickname){
-            alert("no match");
-            return;
+            return res.status(400).json({message: "no match"});
         }
         const del = `delete from todo.user where login_id = ?`;
         db.query(del,[login_id],(err)=>{
             if(err){
                 console.error("withdraw error",err);
-                alert("withdraw error");
-                return;
+                return res.status(500).json({message: "withdraw error"});
             }
             req.session.destroy(()=>{
                 res.json({success: true});
